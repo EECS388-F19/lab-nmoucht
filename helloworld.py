aa93afccd3a23870c83e0957af33b334e7a6276c
Nikos Mouchtaris <nikosm@0587408478.wireless.umich.net>
2019-09-09 16:21:05 -0400

    added script

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+# Your name
+# Two random numbers between 0-100
+# The sum of the two numbers, starting with "Sum = "
+# The average of the two numbers, starting with "Average = "
+import random
+print("Nikos")
+randint1 = random.randint(0,101)
+randint2 = random.randint(0,101)
+
+
+print(randint1)
+print(randint2)
+
+print("Sum = " + str(randint1 + randint2))
+print("Average = " + str((randint1 + randint2)/2)) 
